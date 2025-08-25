706ec20fb19b91d79d5ec4689b3fc1d682c8897b
Rishi Raj Singh <singh34rishi@gmail.com>
2025-08-26 02:18:58 +0530

    Add folder order comments to main function for better organization

--- main.go
+++ main.go
@@ -7,4 +7,12 @@ import (
 
 func main() {
 	fmt.Println("Start of Golang bits....")
+
+	// Folder Order --> 
+	// 1. variables 
+	// 2. conditionals 
+	// 3. functions 
+	// 4. structs 
+	// 5. interfaces 
+	// 6. errors 
 }
